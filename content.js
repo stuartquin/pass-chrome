8ce4c33cf0a1dbc822fcea6ef1f271e1ce27decd
Stuart Quin <stuart.quin@gmail.com>
2015-07-12 20:36:46 +0100

    More complex username field identification

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
-var validUsernameNames = ["email", "user", "username", "login", "id"];
+var validUsernameNames = ["email", "user", "username", "login", "id", "emailaddress"];
+var validUsernameTypes = ["email", "text"];
 var validPasswordNames = ["password", "pass", "passwd"];
 
 var isPasswordField = function(input) {
-  var name = input.name.toLowerCase();
   var type = input.type.toLowerCase();
-  return type === "password" || validPasswordNames.indexOf(name) > -1;
+  return type === "password";
 };
 
 var isUsernameField = function(input) {
@@ -14,15 +14,36 @@ var isUsernameField = function(input) {
 };
 
 var inputs = Array.prototype.slice.call(document.getElementsByTagName("input"), 0 );
+
 var passwordEls = inputs.filter(isPasswordField);
-var usernameEls = inputs.filter(isUsernameField);
+
+
+var getBestUsernameField = function(passwordEl) {
+  var form = passwordEl.form;
+  if (form) {
+    for (var i = 0; i < validUsernameTypes.length; i++) {
+      var type = validUsernameTypes[i]
+      var qry = form.querySelectorAll("input[type='"+type+"']")
+      if (qry.length && qry[0] !== passwordEl) {
+        return qry[0];
+      }
+    }
+  }
+  var usernameEls = inputs.filter(isUsernameField);
+  return usernameEls[0];
+}
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (usernameEls.length) {
-      usernameEls[0].value = request.username;
-    }
+    var passwordEl, usernameEl = null;
+
     if (passwordEls.length) {
-      passwordEls[0].value = request.password;
-    }
+      passwordEl = passwordEls[0];
+      passwordEl.value = request.password;
+      usernameEl = getBestUsernameField(passwordEl);
+
+      if (usernameEl) {
+        usernameEl.value = request.username;
+      }
+    } 
   });
